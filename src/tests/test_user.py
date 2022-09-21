9856e18894ff875e8d968cc038435c32d5175cf9
Natalie Johansson <mnrjohansson@gmail.com>
2022-09-21 08:46:08 +0300

    refactor test_user

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -7,31 +7,25 @@ BASE_URL = 'http://localhost:5000/api'
 connect_to_db()
 
 class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.user = User.create(username="testihenkilö",
+                    password="salainensana",
+                    name="Teppo Testaaja",
+                    email="testiposti@gmail.com",
+                    phone="1928374657")
 
     def test_create_user(self):
-        user = User.create(username="testihenkilö",
-                           password="salainensana",
-                           name="Teppo Testaaja",
-                           email="testiposti@gmail.com",
-                           phone="1928374657")
-        
-        self.assertEqual(user.username, "testihenkilö")
-        self.assertEqual(True, check_password_hash(user.password, "salainensana"))
-        self.assertEqual(user.name, "Teppo Testaaja")
-        self.assertEqual(user.email, "testiposti@gmail.com")
-        self.assertEqual(user.phone, "1928374657")
+        self.assertEqual(self.user.username, "testihenkilö")
+        self.assertEqual(True, check_password_hash(self.user.password, "salainensana"))
+        self.assertEqual(self.user.name, "Teppo Testaaja")
+        self.assertEqual(self.user.email, "testiposti@gmail.com")
+        self.assertEqual(self.user.phone, "1928374657")
 
     def test_to_json(self):
-        user = User.create(username="testihenkilö",
-                           password="salainensana",
-                           name="Teppo Testaaja",
-                           email="testiposti@gmail.com",
-                           phone="1928374657")
-       
-        json = user.to_json()
+        json = self.user.to_json()
         
         self.assertEqual(json, {
-            'id': str(user._id) or None,
+            'id': str(self.user._id) or None,
             'name': "Teppo Testaaja",
             'email': "testiposti@gmail.com",
             'phone': "1928374657",
