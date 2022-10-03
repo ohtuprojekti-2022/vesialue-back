6ab6dea7849a23382358bf67288ad4d9d532e4c2
Jeemlei <eemeli.ingervo@gmail.com>
2022-10-03 16:22:06 +0300

    Coordinate saving to db
    Own collection for areas. Every area has a reference to the inventoryreport it is associated with. Every inventoryreport has a list of areas associated with it.

--- src/models/inventory.py
+++ src/models/inventory.py
@@ -1,11 +1,15 @@
 from pymodm import MongoModel, fields
+from models.area import Area
+
 
 class Inventory(MongoModel):
     """ Class that represents a single inventory.
         Attributes:
-            coordinates: [String] Coordinates of the inventory area.
+            areas: [String] areas of the inventory area.
             inventorydate: [String] Time of the inventory.
-            method: [String] Type of the method used in the inventory.
+            method: [String] sight || echo || dive || other
+            visibility: [String] bad || normal || good
+            method_info: [String] Description of method if method == other
             attachments: [boolean] True if there are attachments included.
             name: [String] The name of the submitter.
             email: [String] The email of the submitter.
@@ -13,9 +17,12 @@ class Inventory(MongoModel):
             more_info: [String] Other notes for the inventory.
     """
 
-    coordinates = fields.CharField(required=True)
+    _id = fields.ObjectId()
+    areas = fields.EmbeddedDocumentListField(Area, blank=True)
     inventorydate = fields.DateTimeField(required=True)
     method = fields.CharField(required=True)
+    visibilit = fields.CharField(blank=True)
+    method_info = fields.CharField(blank=True)
     attachments = fields.BooleanField(required=True, default=False)
     name = fields.CharField(blank=True)
     email = fields.CharField(blank=True)
@@ -27,14 +34,25 @@ class Inventory(MongoModel):
         final = True
 
     @staticmethod
-    def create(coordinates, inventorydate, method, attachments, name="", email="", phone="", more_info=""):
-        inventory = Inventory(coordinates, inventorydate, method, attachments, name, email, phone, more_info)
+    def create(areas, inventorydate, method, visibility="", method_info="",
+               attachments=False, name="", email="", phone="", more_info=""):
+        inventory = Inventory(areas, inventorydate, method, visibility,
+                              method_info, attachments, name, email, phone, more_info)
         inventory.save()
         return inventory
 
+    @staticmethod
+    def update_areas(inventory, new_areas):
+        inventory.areas = new_areas
+        return inventory.save()
+
     def to_json(self):
+        areas = []
+        for area in self.areas:
+            areas.append(area.to_json(simple=True))
         return {
-            'coordinates': self.coordinates,
+            'id': str(self._id),
+            'areas': areas,
             'inventorydate': str(self.inventorydate),
             'method': str(self.method),
             'attachments': str(self.attachments),
@@ -43,4 +61,3 @@ class Inventory(MongoModel):
             'phone': str(self.phone),
             'more_info': str(self.more_info)
         }
-
