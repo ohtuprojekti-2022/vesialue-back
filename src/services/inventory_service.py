aa951200b3d9f185ed51e4cf6ca8ecdf83529228
lukxsx <33071953+lukxsx@users.noreply.github.com>
2022-10-04 13:07:13 +0300

    Validation for inventory report API. Check if fields exist in the JSON request

--- src/services/inventory_service.py
+++ src/services/inventory_service.py
@@ -3,7 +3,7 @@ import datetime
 from werkzeug.exceptions import BadRequest
 from models.inventory import Inventory
 
-COORDINATE_REGEX = '^[NS]([0-8][0-9](\.[0-5]\d){2}|90(\.00){2})\040[EW]((0\d\d|1[0-7]\d)(\.[0-5]\d){2}|180(\.00){2})$'
+COORDINATE_REGEX = r'^[NS]([0-8][0-9](\.[0-5]\d){2}|90(\.00){2})\040[EW]((0\d\d|1[0-7]\d)(\.[0-5]\d){2}|180(\.00){2})$'
 EMAIL_REGEX = r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'
 
 class InventoryService:
@@ -13,21 +13,27 @@ class InventoryService:
         """ Class constructor. Creates a new sight service."""
 
     def add_inventory(self, data):
-        coordinates = data['coordinates']
-        inventorydate = data['inventorydate']
-        method = data['method']
-        attachments = data['attachments']
-        name = data['name']
-        email = data['email']
-        phone = data['phone']
-        more_info = data['more_info']
+        properties = [
+            'coordinates',
+            'inventorydate',
+            'method',
+            'attachments',
+            'name',
+            'email',
+            'phone',
+            'more_info'
+        ]
 
-        self.validate_inventorydate(inventorydate)
-        self.validate_method(method)
-        self.validate_email(email)
+        for key in properties:
+            if not key in data:
+                raise BadRequest(description='invalid request, missing '+key)
 
-        inventory = Inventory.create(coordinates=coordinates, inventorydate=inventorydate, method=method,
-            		  attachments=attachments, name=name, email=email, phone=phone, more_info=more_info)
+        self.validate_inventorydate(data['inventorydate'])
+        self.validate_method(data['method'])
+        self.validate_email(data['email'])
+
+        inventory = Inventory.create(data['coordinates'], data['inventorydate'], data['method'],
+            		  data['attachments'], data['name'], data['email'], data['phone'], data['more_info'])
 
         return inventory.to_json()
 
