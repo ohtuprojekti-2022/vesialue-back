9e0974633e614c4dcdf9db03ffaee493dc2e55f1
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2022-11-09 11:00:24 +0200

    Config conditional api for e2e tests

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,9 +1,10 @@
-# import os
 from flask_restx import Api
 from .inventory_route import api as inventory_api
 from .register_route import api as register_api
 from .login_route import api as login_api
 from .user_route import api as user_api
+from .tests_route import api as tests_api
+from utils.config import ENV, E2E_ENV
 
 api = Api(
     title='Api for Vesialueen inventointi-ilmoitus'
@@ -14,5 +15,5 @@ api.add_namespace(login_api, path='/api/login')
 api.add_namespace(inventory_api, path='/api/inventory')
 api.add_namespace(user_api, path='/api/user')
 
-# if os.getenv('FLASK_ENV') == 'development':
-#     api.add_namespace(tests_api, path='/api/tests')
+if E2E_ENV or ENV == "development":
+    api.add_namespace(tests_api, path='/api/tests')
