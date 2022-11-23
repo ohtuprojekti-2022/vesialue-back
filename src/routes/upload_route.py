5d359f6e20ad04dece0c50c4ca3efa43f21b12c4
lukxsx <33071953+lukxsx@users.noreply.github.com>
2022-11-23 14:20:02 +0200

    Save inventory id alongside the file data

--- src/routes/upload_route.py
+++ src/routes/upload_route.py
@@ -1,14 +1,24 @@
 from flask import request
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
 from flask_restx import Namespace, Resource
+from werkzeug.exceptions import NotFound
 from models.attachment import Attachment
+from models.inventory import Inventory
 
 api = Namespace('upload')
 
 @api.route('')
 class UploadAttachment(Resource):
     def post(self):
-        print(request.files)
+        print(request.form)
         file = request.files['formFile']
         print(file)
-        att = Attachment(file=file.read())
+        try:
+            inventory = Inventory.objects.get(
+                {'_id': ObjectId(request.form['inventory'])})
+        except (Inventory.DoesNotExist, InvalidId) as error:
+            raise NotFound(description='404 not found') from error
+
+        att = Attachment(file=file.read(), inventory=inventory)
         att.save()
